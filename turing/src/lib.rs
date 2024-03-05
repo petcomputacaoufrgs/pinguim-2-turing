b3bbcf335609d0ef177282ff21d84e84b45174cc
Beatriz <110700147+beaarend@users.noreply.github.com>
2024-03-04 22:37:10 -0300

    fix: importing from turing to turing-wasm

--- turing/src/lib.rs
+++ turing/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub mod tests{
+    pub fn capslock(name: &str) -> String {
+        name.to_uppercase()
     }
 }
