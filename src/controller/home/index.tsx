b3eac62c23ad05bc7a64c6239d92bd960fdb468e
Kamille <kamille.kpimentel@gmail.com>
2024-10-01 14:08:43 -0300

    testando autorização

--- src/controller/home/index.tsx
+++ src/controller/home/index.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import init, { add } from "turing-wasm";
-import {Container, ContainerBody} from "./styled.ts"
+/*import init, { add } from "turing-wasm";*/
+import {Container, ContainerBody} from "./styled.ts";
 import Header from '../../components/header/index.tsx';
 import Upload_button from '../../components/upload_button/index.tsx';
 import Inputs from '../../components/input/index.tsx';
 import Documentation from '../../components/documentation/index.tsx';
 
 export function Home() {
-  const [ans, setAns] = useState(0);
+  /*const [ans, setAns] = useState(0);
   useEffect(() => {
     init().then(() => {
       setAns(add(1,1));
     })
-  }, [])
+  }, [])*/
   
   return (
     <Container>
@@ -22,6 +22,8 @@ export function Home() {
         <div id="div1">
           <Upload_button/>
           
+          <div></div>
+
           <div id="div1_part2">
             <div id="div1_part2_inputs">
               <Inputs title="Estado:"/>
