6f2a29c0e4e859a5b653c42c8a5ed7fc3861af89
Eduardo Fonseca da Silva <edufon1503@gmail.com>
2025-01-18 14:59:35 -0300

    Quebra de linha nas mensagens de erro da tabela

--- src/components/transition_error_messages/styles.ts
+++ src/components/transition_error_messages/styles.ts
@@ -4,11 +4,14 @@ import styled from "styled-components"
 export const ErrorText = styled.p`
     color: red;
     font-size: 0.8vw;
+    width: 25%;
+    text-align: center;
 `
 
 export const ErrorsContainer = styled.div`
     background-color: transparent;;
     width: 100%;
     display: flex;
-    justify-content: space-around;
+    justify-content: flex-start;
+    flex-wrap: wrap;
 `
